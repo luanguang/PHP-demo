802a3c0c44423c49c21feb81363a29f2889d64d6
luanguang <523086505@qq.com>
2019-07-26 16:34:03 +0800

    整合成方法

--- fopen_port.php
+++ fopen_port.php
@@ -1,16 +1,20 @@
 <?php
-for ($i = 20180; $i <= 20200; $i++) {
-    $fp = fsockopen("144.202.34.93", $i, $errno, $errstr, 30);
-    if (!$fp) {
-        echo "$errstr ($errno)<br />\n";
-    } else {
-        $out = "GET / HTTP/1.1\r\n";
-        $out .= "Host: www.example.com\r\n";
-        $out .= "Connection: Close\r\n\r\n";
-        fwrite($fp, $out);
-        while (!feof($fp)) {
-            echo fgets($fp, 128);
+
+function searchPort($ip, $startPort = 1, $endPort = 65535) {
+    for ($i = $startPort; $i <= $endPort; $i++) {
+        $fp = @fsockopen($ip, $i, $errno, $errstr, 30);
+        if (!$fp) {
+            echo "$errstr ($errno)\n";
+        } else {
+            $out = "Host: $ip\r\n";
+            $out .= "port: $i\r\n";
+            fwrite($fp, '');
+            while (!feof($fp)) {
+                echo fgets($fp, 128);
+            }
+            echo $out;
+            fclose($fp);
         }
-        fclose($fp);
     }
 }
+
